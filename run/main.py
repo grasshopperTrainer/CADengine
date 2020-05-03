44a5e192a9e964cdc9299d9b0b3f5ca578bdc05c
grasshopperTrainer <grasshoppertrainer@gmail.com>
2020-05-03 18:42:54 +0900

    Implement basic window control.
    Multiple window control, basic packaging.
    
    Used multi threading to handle multiple windows.
    Need to review releasing of occupied memory.

--- run/main.py
+++ run/main.py
@@ -1,5 +1,5 @@
-import window
+from UVT import Window
 
-
-
-
+window1 = Window.new_window(200,200,'first window', monitor=None, shared=None)
+window2 = Window.new_window(1000,1000, 'second window', monitor=None, shared=window1)
+Window.run()
---
+++ src/UVT/__init__.py
@@ -0,0 +1,11 @@
+"""
+Universal Visualization Tool(working title)
+"""
+
+from .env.windows import Windows
+
+class Window(Windows):
+    """
+    Front face of 'windowing' class.
+    """
+    pass
