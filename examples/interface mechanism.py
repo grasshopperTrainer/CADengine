632422834d31af19abe06f915c81c29073fce190
grasshopperTrainer <grasshoppertrainer@gmail.com>
2020-06-29 23:45:38 +0900

    Test noding algorithm
    Test fix noding. Value update detection, connecting, disconnecting nodes, deleting intf tested. Need more testing, think of other relationship actions.

--- examples/interface mechanism.py
+++ examples/interface mechanism.py
@@ -1,9 +1,9 @@
 import weakref as wr
 from collections import namedtuple, deque
+from collections.abc import Iterable
 import inspect
 import warnings
 import heapq
-
 
 class NodeSpvr:
     """
@@ -42,8 +42,7 @@ class NodeSpvr:
         :param inp:
         :return:
         """
-        if out.intf_holder._node_spvr != self:
-            raise AttributeError
+
         l_supv, r_supv = out.intf_holder._node_spvr, inp.intf_holder._node_spvr
         # two graphs are islands so need to be merged beforehand
         is_merged = False
@@ -55,25 +54,26 @@ class NodeSpvr:
             is_merged = True
 
         # check distance and update if needed
+        supv = l_supv
         l_node, r_node = out.intf_holder, inp.intf_holder
 
         # loop check ?
         # check only relationships is being built within an island
         # and right is before left
         if not is_merged:
-            for nxt_node in self._node_rightward(r_node):
+            for nxt_node in supv._node_rightward(r_node):
                 if nxt_node == l_node:
                     warnings.warn("Trying to make a look. Connection not accepted")
                     return
-                if self.dist(nxt_node) > self.dist(l_node):
+                if supv.dist(nxt_node) > supv.dist(l_node):
                     break
 
         # write relationship and calculate distance,
         # push value, push r_node to update que as new value has been just pushed
-        self._relate(l_node, out, inp, r_node)
-        self._graph[r_node]['dist'] = self.dist(l_node) + 1
+        supv._relate(l_node, out, inp, r_node)
+        supv._graph[r_node]['dist'] = supv.dist(l_node) + 1
         inp._intf_obj = out._intf_obj
-        self.push_needto_update(r_node)
+        supv.push_needto_update(r_node)
 
         # update distance of all r_node's rightward
         # no need to align nodes. just que
@@ -81,13 +81,11 @@ class NodeSpvr:
 
         while que:
             node = que.popleft()
-            this_dist = self.dist(node)
-            for _, rights in self.rels(node).items():
-                for right in rights:
-                    that_dist = self.dist(right)
-                    if this_dist >= that_dist:
-                        self._graph[right]['dist'] = this_dist + 1
-                        que.append(right)
+            this_dist = supv.dist(node)
+            for o, i, r_node in supv._node_full_rels(node):
+                if this_dist >= supv.dist(r_node):
+                    supv._graph[r_node]['dist'] = this_dist + 1
+                    que.append(r_node)
 
     def disconnect(self, instance, intf):
         """
@@ -100,19 +98,24 @@ class NodeSpvr:
         # it doesn't violate hierarchy but could be cleaned to maintain dist understandable?
         # TODO: algorithm for maintaining nodes' (attr) dist relatively close
         if intf.intf_sign == Input:
-            for node, rels in self._graph.items():
-                for out, rights in rels.items():
+            print('disconnection', instance, intf)
+            for node, dist_rels in self._graph.items():
+                for out, rights in dist_rels['rels'].items():
                     for right, inps in rights.items():
                         if intf in inps:
                             inps.remove(intf)
+                            if not inps:
+                                del rights[right]
                             break
 
         elif intf.intf_sign == Output:
             d = self.rels(instance)
             if intf in d:
-                for right, inps in d.items():
-                    for inp in inps:
-                        delattr(right, inp)
+                for inp, rights in d.items():
+                    for right, inps in rights.items():
+                        for inp in inps:
+                            # reset r_node's input into default value
+                            delattr(right, inp.intf_name)
                 del d[intf]
         else:
             NotImplementedError
@@ -147,7 +150,7 @@ class NodeSpvr:
         """
         return self._graph[node]['rels']
 
-    def update_rightward(self, end_node=None):
+    def update_tomake_updated(self, target_node=None):
         """
         Build update graph then push values consequently
 
@@ -158,30 +161,67 @@ class NodeSpvr:
         # adding i for dist(n) being the same
         # !Q is _update_que node that has run (def) operate or that has to run operate?
         for nxt_node in self._node_rightward(self._needto_update):
+            # all nodes before end_node can be calculated even if it doesn't affect end_node's value
+            if self.dist(nxt_node) > self.dist(target_node):
+                break
             if nxt_node in self._needto_update: # reset que
                 self._needto_update.remove(nxt_node)
-
             nxt_node.operate()  # run to update outputs
             # push outputs to next node
-            for out, rights in self.rels(nxt_node).items():
-                for right, inps in rights:
-                    for inp in inps:
-                        inp._intf_obj = out._intf_obj
+            for out, inp, r_node in self._node_full_rels(nxt_node):
+                inp._intf_obj = out._intf_obj
 
-            if nxt_node == end_node:
+            if nxt_node == target_node:
                 # push next of nxt_node needto update and stop updating
-                for out, rights in self.rels(nxt_node):
-                    self._needto_update.update(rights.keys())
+                for out, inp, r_node in self._node_full_rels(nxt_node):
+                    self._needto_update.add(r_node)
+                break
 
-    def _node_rightward(self, nodes):
-        if isinstance(nodes, (list, tuple, set)):
-            heap = list([(self.dist(n), i, n) for i, n in enumerate(nodes)])
-            entry = len(nodes)
-            visited = set(nodes)
+    def _graph_full_rels(self):
+        """
+        Generator of all relationships
+
+        :param node: relationship of
+        :return: Generator('full_rel_info', ('output', 'input', 'right_node'))
+        """
+        np = namedtuple('graph_full_rels', ('dist', 'node', 'output', 'input', 'right_node'))
+        for node, dist_rels in self._graph.items():
+            for dist, rels in dist_rels.items():
+                for out, rights in rels.items():
+                    for right, inps in rights.items():
+                        for inp in inps:
+                            yield np(dist, node, out, inp, right)
+
+    def _node_full_rels(self, node):
+        """
+        Generator of relationships of given node
+
+        :param node: relationship of
+        :return: Generator('node_full_rels', ('output', 'input', 'right_node'))
+        """
+        np = namedtuple('node_full_rels', ('output', 'input', 'right_node'))
+        for out, rights in self._graph[node]['rels'].items():
+            for right, inps in rights.items():
+                for inp in inps:
+                    yield np(out, inp, right)
+
+    def _node_rightward(self, roots):
+        """
+        Generator of rightward nodes
+
+        Returns rightwards nodes of given roots in hierarchical order
+        :param roots: Nodes tracking from. Multiple nodes can be given.
+        :return: Generator
+        """
+
+        if isinstance(roots, Iterable):
+            heap = list([(self.dist(n), i, n) for i, n in enumerate(roots)])
+            entry = len(roots)
+            visited = set(roots)
         else:
-            heap = [(self.dist(nodes), 0, nodes)]
+            heap = [(self.dist(roots), 0, roots)]
             entry = 1
-            visited = {nodes}
+            visited = {roots}
 
         heapq.heapify(heap)
         while heap:
@@ -222,7 +262,7 @@ class IntfObj:
         self._intf_obj = value  # real value wrapped
 
     def __str__(self):
-        return f"< {self._intf_sign.__name__} : {self._intf_obj} >"
+        return f"< {self._intf_sign.__name__} : {type(self._intf_obj).__name__} : {self._intf_obj} >"
 
     @property
     def intf_name(self):
@@ -248,19 +288,35 @@ class IntfObj:
         return self._intf_obj
 
     def __add__(self, other):
+        if isinstance(other, IntfObj):
+            return self._intf_obj.__add__(other._intf_obj)
         return self.real_value.__add__(other)
 
     def __sub__(self, other):
-        return self.real_value.__sub__(other)
+        if isinstance(other, IntfObj):
+            return self._intf_obj.__sub__(other._intf_obj)
+        return self._intf_obj.__sub__(other)
 
     def __truediv__(self, other):
-        return self.real_value.__truediv__(other)
+        if isinstance(other, IntfObj):
+            return self._intf_obj.__truediv__(other._intf_obj)
+        return self._intf_obj.__truediv__(other)
 
     def __floordiv__(self, other):
-        return self.real_value.__floordiv__(other)
+        if isinstance(other, IntfObj):
+            return self._intf_obj.__floordiv__(other._intf_obj)
+        return self._intf_obj.__floordiv__(other)
 
     def __mul__(self, other):
-        return self.real_value.__mul__(other)
+        if isinstance(other, IntfObj):
+            return self._intf_obj.__mul__(other._intf_obj)
+        return self._intf_obj.__mul__(other)
+
+    def __getitem__(self, item):
+        return self._intf_obj.__getitem__(item)
+
+    def __getattr__(self, item):
+        return getattr(self._intf_obj, item)
 
 
 class IntfDescriptor:
@@ -269,11 +325,12 @@ class IntfDescriptor:
 
     To control setting value
     """
-    _sign = None
 
-    def __init__(self, name, def_val):
-        self._name = name
-        self._record_name = f'_{self._sign}_{name}'
+    def __init__(self, def_val):
+        # parse initing code line identify name
+        c = inspect.getframeinfo(inspect.currentframe().f_back).code_context[0]
+        self._name = c.split(self.__class__.__name__)[0].strip().split('=')[0].strip()
+        self._record_name = f'_{self.__class__.__name__}_{self._name}'
         self._def_val = def_val
 
     def __set__(self, instance, value):
@@ -298,8 +355,8 @@ class IntfDescriptor:
         :param owner:
         :return:
         """
-        self._init_interface(instance)
-        instance._node_spvr.update_rightward(instance)
+        self._check_init(instance)
+        instance._node_spvr.update_tomake_updated(instance)
         return getattr(instance, self._record_name)
 
     def __delete__(self, instance):
@@ -312,12 +369,12 @@ class IntfDescriptor:
         :param instance:
         :return:
         """
-        self._init_interface(instance)
+        self._check_init(instance)
         instance._node_spvr.disconnect(instance, getattr(instance, self._record_name))
         intf_obj = IntfObj(instance, self._name, type(self), self._def_val)
         setattr(instance, self._record_name, intf_obj)
 
-    def _init_interface(self, instance):
+    def _check_init(self, instance):
         """
         Initiate interface
 
@@ -344,7 +401,6 @@ class Input(IntfDescriptor):
     """
     Designate input value
     """
-    _sign = 'input'
 
     def __set__(self, instance, value):
         """
@@ -354,11 +410,13 @@ class Input(IntfDescriptor):
         :param value:
         :return:
         """
-        self._init_interface(instance)
+        self._check_init(instance)
 
         if isinstance(value, IntfObj):  # if connecting node to node
             if value.intf_sign == Input: # if connecting input -> input interfaces
                 raise AttributeError("direction should be (output) -> (input)")
+
+            instance._node_spvr.disconnect(instance, getattr(instance, self._record_name))
             instance._node_spvr.build_rel(value, getattr(instance, self._record_name))
 
         else:  # if putting raw value
@@ -372,7 +430,6 @@ class Output(IntfDescriptor):
 
     Manages setting getting deleting output value
     """
-    _sign = 'output'
 
     def __set__(self, instance, value):
         """
@@ -387,7 +444,7 @@ class Output(IntfDescriptor):
         :param value:
         :return:
         """
-        self._init_interface(instance)
+        self._check_init(instance)
         # guess if assigning value is called inside the node
         calling_frame = inspect.currentframe().f_back
         frameinfo = inspect.getframeinfo(calling_frame)
@@ -409,147 +466,116 @@ class Output(IntfDescriptor):
         raise AttributeError("Output can't be set explicitly")
 
 
-class K:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-
-class A:
-    a = Input('a', 0)
-    a_out = Output('a_out', 0)
-
-    def operate(self):
-        self.a_out = self.a
-        print('operation A')
-
-
-class B:
-    b = Input('b', 0)
-    c = Output('c', 0)
-
-    def operate(self):
-        self.c = self.b * 2
-        print('operation B')
-
 
 if __name__ == '__main__':
-    def simple_task():
-        a = A()
-        b = B()
-        print('setting value explicitly')
-        a.a = 10
-        print(a.a)
-        print(a.a_out)
-        print(a.a_out)
-        # setting reverse
-        # a.a_out = b.b
 
-        print('simple connection')
-        b.b = a.a_out
-        print(b.b)
-        print('check operation')
-        print(b.c)
-        print('check if operation is not called when no change made')
-        print(b.c)
-        print(b.b)
-        print('check update chained')
-        a.a = 15
-        print(b.b)
-        print(b.c)
-        print('check disconnection from input')
-        del b.b
-        print(a.a)
-        print(a.a_out)
-        print(b.c)
-        print('check disconnection from output')
-        a.a = 100
-        b.b = a.a_out
-        print(b.c)
-        del a.a_out
+    class K:
+        def __init__(self, a, b):
+            self.a = a
+            self.b = b
+
+
+    class A:
+        i = Input(None)
+        o = Output(None)
+
+        def operate(self):
+            self.o = self.i
+            print('operation A')
+
+
+    class B:
+        first_in = Input('')
+        second_in = Input('')
+        o = Output(0)
+
+        def operate(self):
+            print('operation B')
+            self.o = self.first_in + self.second_in
+
+    class Upper:
+        i = Input(None)
+        o = Output(None)
+
+        def operate(self):
+            print('operate Upper')
+            self.o = self.i.upper()
+
+    class FirstLast:
+        i = Input(None)
+        o1 = Output(None)
+        o2 = Output(None)
+
+        def operate(self):
+            print('operate FirstLast')
+            self.o1 = self.i[0]
+            self.o2 = self.i[-1]
+
+    class Join:
+        i1 = Input(None)
+        i2 = Input(None)
+        o = Output(None)
+
+        def operate(self):
+            print('operate Join')
+            self.o = self.i1 + self.i2
+
+
+    def text_two_nodes():
+
+        node1, node2 = A(), B()
+        print(node1.i, node1.o)
+        node1.i = 'first in_'
+        print(node1.o)
         print()
-        print(a.a_out)
-        print(b.c)
+        node2.first_in = node1.o
+        print(node2._node_spvr._needto_update)
+        node2.second_in = node1.o
+        print('inputing two values done')
+        print()
+        node1.i = 'other in'
+        print(node2.o)
+        node2.second_in = 'this is second in'
+        print(node2.o)
+        node1.i = node2.o
+        print()
+        print()
+        del node1.o
+        print(node2.first_in)
+        print(node1.o)
 
-    def three_node():
-        class A:
-            i = Input('i', None)
-            o = Output('o', None)
-
-            def operate(self):
-                print('Operate A')
-                self.o = self.i
-
-        class B:
-            i = Input('i', None)
-            o = Output('o', None)
-
-            def operate(self):
-                print('Operate B')
-                self.o = self.i
-
-        class C:
-            i = Input('i', None)
-            o = Output('o', None)
-
-            def operate(self):
-                print('Operate C')
-                self.o = self.i
-
-        a, b, c = A(), B(), C()
-        c.i = b.o
-        b.i = a.o
-        a.i = 10
-        print(c.o)
-        a.i = 20
-        print(c.o)
-
-    def rebuild():
-        class A:
-            i = Input('i', 0)
-            o = Output('o', None)
-
-            def operate(self):
-                print('operating A')
-                self.o = self.i + 1
-
-        a = A()
-        print('input of a', a.i, a._node_spvr)
-        print('Output of a', a.o)
-        del a.o
-        b = A()
-        b.i = a.o
-
-        # print('Output of a', a.o)
-        # b = A()
-        # print('connect')
-        # b.i = a.o
-        # print(a._node_spvr._graph)
-        # print()
-        # print('new value set')
-        # a.i = 20
-        # # print(a._node_spvr, b._node_spvr)
-        # # print(a._node_spvr._update_que)
-        # print('--------------------')
-        # print(b._node_spvr._graph)
-        # print(b.o)
-        # b.i = 100
-        # print(b.o)
-        # print(a.i, a.o, b.i, b.o)
-        # b.i = a.o
-        # a.i = 1000
-        # print(a.i, a.o, b.i, b.o)
-        # print(a._node_spvr._update_que)
-        # print(b._node_spvr._update_que)
-        # print(b.i, b._node_spvr._update_que)
-        # print(a._node_spvr, b._node_spvr)
-        # print(b._node_spvr._graph)
-        # a.i = 10
-        # print(b.o)
-        # c = A()
-        # c.i = b.o
-        # print(c.i, c.o)
-
-
-
-    rebuild()
+    def test_three_nodes():
+        u, fl, j = Upper(), FirstLast(), Join()
+        u.i = 'abc'
+        fl.i = u.o
+        j.i1 = fl.o1
+        j.i2 = fl.o2
+        print(j.o)
+        j.i2 = u.o
+        print(j.i1, j.i2, j.o)
+        del j.i1
+        j.i1 = 'new'
+        print(j.o)
+        fl.i = 'fff'
+        print('ddd')
+        j.i2 = fl.o1
+        print(j.i1, j.i2)
+        print(u.o)
+        print(j.o)
+        print(j._node_spvr._needto_update)
+        print(j.o)
+        u.i = 'ddddd'
+        for i in j._node_spvr._node_full_rels(u):
+            print(i)
+        print(u.o)
+        j.i1 = j.i2 = u.o
+        print(j.o)
+        u.i = '233'
+        j.i2 = fl.o1
+        u.i = fl.i = 'kk'
+        print(j.o)
+        print()
+        del u.i
+        del j.i1, j.i2
+        print(j.o)
+    test_three_nodes()
