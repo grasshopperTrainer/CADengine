c9aea5b7c80eec10436868cf45e150074d38e910
grasshopperTrainer <grasshoppertrainer@gmail.com>
2021-02-15 22:47:27 +0900

    pyopengl2-34 consider implement color id intersection
    implement EntityRegistry
        Random used to generate color id. There must be packed registry handling mechanism but is not necessary for now. Just to think, simple expanding registry bitsize will do the job - dont know if using some hasing method is better than generating random color.

--- src/mkernel/shapes/brep_wrapper.py
+++ src/mkernel/shapes/brep_wrapper.py
@@ -1,8 +1,12 @@
+import random
+import weakref as wr
+
 import numpy as np
 from ckernel.render_context.opengl_context.bffr_cache import BffrCache
 from mkernel.shapes.base import Shape
 import gkernel.dtype.geometric as gt
 from mkernel.renderers.brep_renderer import BrepRenderer
+from gkernel.color import *
 
 
 class Brep(Shape):
@@ -16,7 +20,9 @@ class Brep(Shape):
 
         # self.__crv_cache = BffrCache(dtype=np.dtype([('clr', 'f4', 4), ('thk', 'f4')]), locs=(0, 1), size=16)
         # self.__srf_cache =
-        self.__vrtx_blocks = {}
+        self.__points = set()
+
+        self.__entity_registry = EntityRegistry()
 
     @property
     def geo(self):
@@ -33,16 +39,15 @@ class Brep(Shape):
         :param z:
         :return:
         """
-        v, p = self.__geo.addget_vrtx(x, y, z)
-        block = self.__vrtx_cache.request_block(size=1)
-        block['coord'] = p.T
-        bb = self.__pnt_cache.request_block(size=1)
-        bb['dia'] = 5
-        bb['clr'] = 1, 1, 1, 1
-        bbb = self.__pnt_indx_cache.request_block(size=1)
-        bbb['idx'] = block.indices
-        self.__vrtx_blocks[v] = block
-        return v
+        vrtx = self.__geo.addget_vrtx(x, y, z)
+        vrtx_block = self.__vrtx_cache.request_block(size=1)
+        point_vrtx_block = self.__pnt_cache.request_block(size=1)
+        point_indx_block = self.__pnt_indx_cache.request_block(size=1)
+
+        point = PointRenderEntity(vrtx, vrtx_block, point_vrtx_block, point_indx_block)
+        self.__entity_registry.getset_entity_cid(point)
+        self.__points.add(point)
+        return vrtx
 
     def add_lin(self):
         raise NotImplementedError
@@ -50,10 +55,62 @@ class Brep(Shape):
     def add_pgon(self):
         raise NotImplementedError
 
+
+class EntityRegistry:
+
+    def __init__(self):
+        self.__record = wr.WeakKeyDictionary()
+
+        self.__color_comp_bitsize = 8
+        self.__color_comp_bitmask = [0 for i in range(3)]
+
+    def getset_entity_cid(self, entity):
+        """
+        get, set color id for the entity
+
+        :param entity:
+        :return:
+        """
+        if entity in self.__record:
+            return self.__record[entity]
+        else:
+            while True:
+                color_comps = tuple(random.getrandbits(self.__color_comp_bitsize) for _ in range(3))
+                # check for reserved 0
+                if any([cc == 0 for cc in color_comps]):
+                    continue
+
+                masked_count = 0
+                for bm, cc in zip(self.__color_comp_bitmask, color_comps):
+                    if bm & (1 << cc):
+                        masked_count += 1
+                # color is already occupied
+                if masked_count == 3:
+                    continue
+                else:   # mask
+                    for i in range(3):
+                        self.__color_comp_bitmask[i] |= color_comps[i]
+                    break
+            self.__record[entity] = color_comps
+            return color_comps
+
 class RenderEntity:
     pass
 
 
 class PointRenderEntity(RenderEntity):
-    def __init__(self, vrtx):
-        pass
+    def __init__(self, vrtx, coord_block, vrtx_block, indx_block):
+        """
+        gonna render given vertex
+
+        :param vrtx:
+        """
+        self.__vrtx = vrtx
+        self.__coord_block = coord_block
+        self.__vrtx_block = vrtx_block
+        self.__indx_block = indx_block
+
+        self.__coord_block['coord'] = vrtx.point.T
+        self.__vrtx_block['clr'] = ClrRGBA(1, 1, 1, 1)
+        self.__vrtx_block['dia'] = 10
+        self.__indx_block['idx'] = self.__coord_block.indices
