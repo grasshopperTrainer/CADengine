696170f160909150251e452d6e22d157fed97f56
grasshopperTrainer <grasshoppertrainer@gmail.com>
2021-03-03 18:37:09 +0900

    pyopengl2-77 implement plane renderer
    implement thick line plane
        resolve #77

--- src/mkernel/renderers/shaders/plane_geom_shdr.glsl
+++ src/mkernel/renderers/shaders/plane_geom_shdr.glsl
@@ -1,7 +1,7 @@
 #version 450 core
 
 layout (points) in;
-layout (line_strip, max_vertices = 18) out;// three lines for axes
+layout (triangle_strip, max_vertices = 12) out;// three lines for axes
 
 layout (location=0) uniform mat4 PM;
 layout (location=1) uniform mat4 VM;
@@ -16,6 +16,7 @@ out vec4 clr;
 
 // default colors
 const vec4 origin = vs_in[0].pln[0];
+const float thk = 0.02;
 const mat4 TM = PM * VM * MM;
 const vec4 clrs[3] = vec4[3](vec4(1, 0, 0, 1), vec4(0, 1, 0, 1), vec4(0, 0, 1, 1));
 const vec4 vectors[3] = vec4[3](vs_in[0].pln[1], vs_in[0].pln[2], vs_in[0].pln[3]);
@@ -23,29 +24,18 @@ const vec4 vectors[3] = vec4[3](vs_in[0].pln[1], vs_in[0].pln[2], vs_in[0].pln[3
 void draw_axis(float scale, int i) {
     clr = clrs[i];
 
-    mat2x4 vertices = mat2x4(origin, vs_in[0].pln[0] + vectors[i] * scale);
-    gl_Position = TM * vertices[0];
-    EmitVertex();
-    gl_Position = TM * vertices[1];
-    EmitVertex();
-    EndPrimitive();
+    // calculate offset normal
+    vec3 norm = normalize(cross(vec3(0, 0, -1), (VM * MM * vectors[i]).xyz));
+    vec4 off = inverse(VM * MM) * vec4(norm, 0) * thk * scale;
+    vec4 end = origin + vectors[i] * scale;
 
-    mat4 MoveM = mat4(1.0);
-    MoveM[3] = vectors[(i+1)%3] * scale * 0.005;
-    MoveM[3][3] = 1;
-
-    gl_Position = TM * MoveM * vertices[0];
+    gl_Position = TM * (origin + off);
     EmitVertex();
-    gl_Position = TM * MoveM * vertices[1];
+    gl_Position = TM * (origin - off);
     EmitVertex();
-    EndPrimitive();
-
-    MoveM[3] = vectors[(i+1)%3] * scale * -0.005;
-    MoveM[3][3] = 1;
-
-    gl_Position = TM * MoveM * vertices[0];
+    gl_Position = TM * (end + off);
     EmitVertex();
-    gl_Position = TM * MoveM * vertices[1];
+    gl_Position = TM * (end - off);
     EmitVertex();
     EndPrimitive();
 }
@@ -54,7 +44,6 @@ void main() {
     // using camera distance for constant sizing
     // ambiguous length unit but leave it for now
     float scale = length(VM*MM*vs_in[0].pln[0])*vs_in[0].len;
-    vec4 origin = PM * VM * MM * vs_in[0].pln[0];
 
     draw_axis(scale, 0);
     draw_axis(scale, 1);
